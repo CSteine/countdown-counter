749df04d17999b2966d4bf80af18f90861750766
CSteine <cmstein09@icloud.com>
2019-01-13 14:06:43 +0000

    Committing Completed Files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Clock from './Clock';
 import './App.css';
+import { Form, FormControl, Button } from 'react-bootstrap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'December 25, 2017',
+      newDeadline: ''
+    }
+  }
+
+  changeDeadline() {
+    this.setState({deadline: this.state.newDeadline});
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="App-title">
+          Countdown to {this.state.deadline}
+        </div>
+        <Clock
+          deadline={this.state.deadline}
+        />
+        <Form inline>
+          <FormControl
+            className="Deadline-input"
+            placeholder='new date'
+            onChange={event => this.setState({newDeadline: event.target.value})}
+          />
+          <Button onClick={() => this.changeDeadline()}>
+            Submit
+          </Button>
+        </Form>
       </div>
-    );
+    )
   }
 }
 
